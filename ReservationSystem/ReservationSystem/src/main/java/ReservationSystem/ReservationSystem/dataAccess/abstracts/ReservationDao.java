67a3ad93579e7d293ae9144d066b50986a803b84
Aysu Sözen <78969561+aysussozenn@users.noreply.github.com>
2021-09-01 14:30:41 +0300

    Delete ReservationSystem/ReservationSystem directory

--- ReservationSystem/ReservationSystem/src/main/java/ReservationSystem/ReservationSystem/dataAccess/abstracts/ReservationDao.java
+++
@@ -1,9 +0,0 @@
-package ReservationSystem.ReservationSystem.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ReservationSystem.ReservationSystem.entity.concretes.Reservations;
-
-public interface ReservationDao extends JpaRepository<Reservations, Integer> {
-
-}
--- ReservationSystem/ReservationSystem/src/main/java/ReservationSystem/ReservationSystem/dataAccess/abstracts/TableDao.java
+++
@@ -1,8 +0,0 @@
-package ReservationSystem.ReservationSystem.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ReservationSystem.ReservationSystem.entity.concretes.*;
-
-public interface TableDao extends JpaRepository<Tables, Integer> {
-
-}
